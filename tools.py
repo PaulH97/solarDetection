626baabba24cdddb04740c732f4fd44f3c2a154a
paulhoehn <paul-hoehn@outlook.de>
2022-09-22 12:54:26 +0200

    Added new functions

--- tools.py
+++ tools.py
@@ -7,39 +7,14 @@ import numpy as np
 import geopandas as gdp
 import gdal
 import os
-
-vv = r"D:\Universität\Master_GeoInfo\Masterarbeit\data\Sentinel1\2021\CARD_BS_MC\33UVT\S1_CARD-BS-MC_202110_33UVT_VV.tif"
-b2 = r"D:\Universität\Master_GeoInfo\Masterarbeit\data\Sentinel2\L3_WASP\2021\33UVT\SENTINEL2X_20210915-000000-000_L3A_T33UVT_C_V1-2_FRC_B2.tif"
-out = r"D:\Universität\Master_GeoInfo\Masterarbeit\data\Sentinel1\S1_CARD-BS-MC_202110_33UVT_VV_align.tif"
-
-
-def resizeRaster(input_raster, output_path, raster_sample):
-    """
-    This function uses a sample raster to resize a raster file
-    """
-    with rio.open(input_raster) as src:
-
-        name = os.path.basename(input_raster).split('.')[0] + "_resize.tif"    
-
-        # store necessary information from sample raster
-        raster_sample = rio.open(raster_sample)
-        window = Window(0,0,raster_sample.width,raster_sample.height)
-
-        # save content of raster as array for writing process
-        array = src.read(1, window=window)
-        
-        # Update profile for new raster
-        profile = src.profile
-        profile['width'] = raster_sample.width
-        profile['height'] = raster_sample.height
-        profile['transform'] = src.window_transform(window)
-
-
-        output = os.path.join(output_path, name)
-
-        with rio.open(output, 'w', **profile) as dataset:
-            dataset.write_band(1, array)
-            print(f"Changed raster size from: {src.width, src.height} to {dataset.width, dataset.height}.")
+from sklearn.preprocessing import MinMaxScaler
+from rasterio import Affine
+import tifffile as tiff
+import random
+from matplotlib import pyplot as plt
+import requests
+import geopandas as gpd
+from keras import backend as K
 
 
 def load_img_as_array(path):
@@ -60,21 +35,22 @@ def resampleRaster(raster_path, output_folder, resolution):
     output_file = os.path.join(output_folder, name + "_resample.tif")
 
     raster = gdal.Open(raster_path)
-    ds = gdal.Warp(output_file, raster, xRes=resolution, yRes=resolution, resampleAlg="bilinear", format="GTiff")
+    #ds = gdal.Warp(output_file, raster, xRes=resolution, yRes=resolution, resampleAlg="bilinear", format="GTiff")
+    ds = gdal.Warp('', raster, xRes=resolution, yRes=resolution, resampleAlg="bilinear", format="VRT")
 
-    return
+    return ds
 
-def rasterizeShapefile(raster_path, vector_path, output_path):
+def rasterizeShapefile(raster_path, vector_path, output_folder, col_name):
     
     raster = rasterio.open(raster_path)
     vector = gdp.read_file(vector_path)
+    vector = vector.to_crs(raster.crs)
     
-    geom_value = ((geom,value) for geom, value in zip(vector.geometry, vector['gridcode']))
-    osm_res = 10
-    crs    = rasterio.crs.CRS.from_epsg(vector.crs.to_epsg())
+    geom_value = ((geom,value) for geom, value in zip(vector.geometry, vector[col_name]))
+    crs    = raster.crs
     transform = raster.transform
 
-    r_out = os.path.join(output_path, os.path.basename(vector_path).split(".")[0] +".tif")
+    r_out = os.path.join(output_folder, os.path.basename(vector_path).split(".")[0] +".tif")
     with rasterio.open(r_out, 'w+', driver='GTiff',
             height = raster.height, width = raster.width,
             count = 1, dtype="int16", crs = crs, transform=transform) as rst:
@@ -82,3 +58,225 @@ def rasterizeShapefile(raster_path, vector_path, output_path):
         rasterized = features.rasterize(shapes=geom_value, fill=0, out=out_arr, transform = rst.transform)
         rst.write_band(1, rasterized)
     rst.close()
+
+def patchifyRasterAsArray(array, patch_size):
+
+    patches = patchify(array, (patch_size, patch_size, 1), step=patch_size)    
+    patchX = patches.shape[0]
+    patchY = patches.shape[1]
+    scaler = MinMaxScaler()
+    
+    result = []
+
+    for i in range(patchX):
+        for j in range(patchY):
+        
+            single_patch_img = patches[i,j,:,:]
+            # need to normalize values between 0-1
+            single_patch_img = scaler.fit_transform(single_patch_img.reshape(-1, single_patch_img.shape[-1])).reshape(single_patch_img.shape)
+            single_patch_img = single_patch_img[0] #Drop the extra unecessary dimension that patchify adds.                               
+            result.append(single_patch_img)
+    
+    return result
+
+
+def savePatches(patches, output_folder, raster_path):
+
+    sentinel_tile = os.path.basename(raster_path).split("_")[3]
+    mask_out = os.path.join(output_folder, "Crops", "mask") 
+    img_out = os.path.join(output_folder, "Crops", "img") 
+
+    for idx, mask in enumerate(patches["SolarParks"]):
+
+        if  np.count_nonzero(mask == 1):
+            
+            tiff.imwrite(os.path.join(mask_out, f'{sentinel_tile}_mask_{idx}_pv.tif'), mask)
+
+            raster_muster = rasterio.open(raster_path)
+            #transform_xy = raster_muster.transform * col_row
+            #transform = Affine.translation(xoff=transform_xy[0], yoff=transform_xy[1]) * Affine.scale(10,10)
+            #crs = raster_muster.crs
+
+            final = rasterio.open(os.path.join(img_out, f'{sentinel_tile}_img_{idx}_pv.tif'),'w', driver='Gtiff',
+                            width=patches["B2"][0].shape[0], height=patches["B2"][0].shape[1],
+                            count=12,
+                            #crs=crs,
+                            #transform=transform,
+                            dtype=rasterio.float64)
+        
+            final.write(patches["B2"][idx][:,:,0],1) 
+            final.write(patches["B3"][idx][:,:,0],2) 
+            final.write(patches["B4"][idx][:,:,0],3) 
+            final.write(patches["B5"][idx][:,:,0],4)    
+            final.write(patches["B6"][idx][:,:,0],5)
+            final.write(patches["B7"][idx][:,:,0],6)
+            final.write(patches["B8"][idx][:,:,0],7)
+            final.write(patches["B8A"][idx][:,:,0],8)
+            final.write(patches["B11"][idx][:,:,0],9) 
+            final.write(patches["B12"][idx][:,:,0],10)
+            final.write(patches["VV"][idx][:,:,0],11)
+            final.write(patches["VH"][idx][:,:,0],12)
+            final.close()
+
+    
+    return img_out, mask_out
+
+    # random_idx = random.sample(idx_noPV,k=len(idx_PV))
+
+    # for idx, label in enumerate(labels):
+
+    #     if idx in random_idx:
+
+    #         tiff.imwrite(os.path.join(mask_out, f'{id}_mask_{idx}_nopv.tif'), label)
+
+    #         raster_muster = rasterio.open(sen2_sen1_mask[0])
+    #         #transform_xy = raster_muster.transform * col_row
+    #         #transform = rasterio.transform.from_origin(transform_xy[0], transform_xy[1], xsize=10, ysize=10)
+    #         #crs = raster_muster.crs
+
+    #         final = rasterio.open(os.path.join(img_out, f'{id}_img_{idx}_nopv.tif'),'w', driver='Gtiff',
+    #                         width=patch_size, height=patch_size,
+    #                         count=5,
+    #                         #crs=crs,
+    #                         #transform=transform,
+    #                         dtype=rasterio.float32)
+        
+    #         final.write(red[idx][:,:,0],1) # red
+    #         final.write(green[idx][:,:,0],2) # green
+    #         final.write(blue[idx][:,:,0],3) # blue
+    #         final.write(vv[idx][:,:,0],4) # vv
+    #         final.write(vh[idx][:,:,0],5) # vh
+    #         final.close()
+
+def imageAugmentation(images_path, masks_path):
+
+    def rotation90(image, seed):
+        random.seed(seed)
+        r_image = np.rot90(image)
+        return r_image
+
+    def h_flip(image, seed):
+        random.seed(seed)
+        hflipped_img= np.fliplr(image)
+        return hflipped_img
+
+    def v_flip(image, seed):
+        random.seed(seed)
+        vflipped_img= np.flipud(image)
+        return vflipped_img
+
+    def v_transl(image, seed):
+        random.seed(seed)
+        n_pixels = random.randint(-128,128)
+        vtranslated_img = np.roll(image, n_pixels, axis=0)
+        return vtranslated_img
+
+    def h_transl(image, seed):
+        random.seed(seed)
+        n_pixels = random.randint(-128,128)
+        htranslated_img = np.roll(image, n_pixels, axis=1)
+        return htranslated_img
+
+    transformations = {'rotate': rotation90, 'horizontal flip': h_flip,'vertical flip': v_flip, 'vertical shift': v_transl, 'horizontal shift': h_transl}         
+
+    images=[] 
+    masks=[]
+
+    for im in os.listdir(images_path):      
+        images.append(os.path.join(images_path,im))
+
+    for msk in os.listdir(masks_path):  
+        masks.append(os.path.join(masks_path,msk))
+
+    for i in range(len(masks)): 
+        
+        image = images[i]
+        mask = masks[i]
+
+        original_image = load_img_as_array(image)
+        original_mask = load_img_as_array(mask)
+        
+        for idx, transformation in enumerate(list(transformations)): 
+
+            seed = random.randint(1,100)  #Generate seed to supply transformation functions. 
+            transformed_image = transformations[transformation](original_image, seed)
+            transformed_mask = transformations[transformation](original_mask, seed)
+
+            if idx == 2:
+                rows, cols = 2, 2
+                plt.figure(figsize=(12,12))
+
+                plt.subplot(rows, cols, 1)
+                plt.imshow(original_image[:,:,:3])
+                plt.subplot(rows, cols, 2)
+                plt.imshow(transformed_image[:,:,:3])
+                plt.subplot(rows, cols, 3)
+                plt.imshow(original_mask)
+                plt.subplot(rows, cols, 4)
+                plt.imshow(transformed_mask)
+                plt.show()
+
+            new_image_path= image.split(".")[0] + "_aug{}.tif".format(idx)
+            new_mask_path = mask.split(".")[0] + "_aug{}.tif".format(idx)
+            
+            new_img = rasterio.open(new_image_path,'w', driver='Gtiff',
+                        width=transformed_image.shape[0], height=transformed_image.shape[1],
+                        count=12,
+                        dtype=rasterio.float32)
+            
+            for band in range(transformed_image.shape[-1]-1):
+                new_img.write(transformed_image[:,:,band], band+1)
+            new_img.close() 
+            
+            tiff.imwrite(new_mask_path, transformed_mask)
+    return
+
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    coef = (2. * intersection + K.epsilon()) / (K.sum(y_true_f) + K.sum(y_pred_f) + K.epsilon())
+    return coef
+
+
+def ScenceFinderAOI(shape_path, satellite, processing_level, product_type, start_date, end_date, cloud_cover, output_format='json', maxRecords = 15):
+
+    start_date = start_date.replace(":", "%3A") 
+    end_date = end_date.replace(":", "%3A") 
+
+    shape = gpd.read_file(shape_path)
+    shape_4326 = shape.to_crs(epsg=4326)
+    
+    #wkt_list = list(shape_4326['geometry'][0].exterior.coords)
+    # for idx, point in enumerate(wkt_list):
+    #     s = s + str(point[0]) + "+" + str(point[1])
+    #     if idx != len(wkt_list)-1:
+    #         s = s + "%2C"
+    #     else:
+    #         pass
+    shape_wkt = shape_4326.to_wkt()
+    wkt_list = list(shape_wkt['geometry'])
+
+    list_path = []
+
+    for point in wkt_list:
+
+        geometry = point    
+
+        base_url = "http://finder.code-de.org/resto/api/collections/"
+
+        if satellite == "Sentinel1":
+            modified_url = f"{satellite}/search.{output_format}?{maxRecords}&startDate={start_date}&completionDate={end_date}&location=all&processingLevel={processing_level}&productType={product_type}&sortParam=startDate&sortOrder=descending&status=all&geometry={geometry}&dataset=ESA-DATASET"
+        else:
+            modified_url = f"{satellite}/search.{output_format}?{maxRecords}&startDate={start_date}&completionDate={end_date}&cloudCover=[0%2C{cloud_cover}]&location=all&processingLevel={processing_level}&productType={product_type}&sortParam=startDate&sortOrder=descending&status=all&geometry={geometry}&dataset=ESA-DATASET"
+
+        url = base_url + modified_url
+
+        resp = requests.get(url).json()
+
+        for feature in resp['features']:
+            list_path.append(feature['properties']['productIdentifier'])
+
+    return list_path
+
+
